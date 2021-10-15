4ab80edce03bb083a467e8451e46b12a79f91e4b
Toby O'Connell <tobyoconnell@hotmail.com>
2021-10-15 22:34:35 +0100

    Create and use InitialCoordinator

---
+++ Coordinator/Navigation/InitialCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  InitialCoordinator.swift
+//  Coordinator
+//
+//  Created by tobyoconnell on 15/10/2021.
+//
+
+import UIKit
+
+class InitialCoordinator: Coordinating {
+    weak var navigationController: UINavigationController?
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        navigationController?.pushViewController(ViewController(), animated: false)
+    }
+}
